a07d8fe6cb6d8bde42a8a366ff71b085e99ea63f
lewis-cressey <63366182+lewis-cressey@users.noreply.github.com>
2022-11-20 14:36:27 +0100

    Added test: python1

---
+++ python1/main.py
@@ -0,0 +1,321 @@
+from browser import document, window
+import traceback
+import io
+import random
+from types import SimpleNamespace
+
+page = SimpleNamespace()
+
+class Question:
+    instances = []
+
+    @classmethod
+    def instance(self):
+        current_index = page.question_select.selectedIndex
+        return self.instances[current_index]
+    
+    def __init__(self, title, html, use_turtle=False):
+        qid = 1 + len(self.instances)
+        self.name = f"q{qid}"
+        self.title = title
+        self.html = html
+        self.use_turtle = use_turtle
+        self.is_complete = False
+        self.input_queue = []
+        self.output_words = []
+
+        self.option_element = document.createElement("option")
+        page.question_select.append(self.option_element)
+        self.instances.append(self)
+        self.update_option()
+
+    def update_option(self):
+        if self.is_complete: checkbox = "☑ "
+        else: checkbox = "☐ "
+        self.option_element.textContent = f"{checkbox} {self.title}"
+
+    def clear(self):
+        self.input_queue.clear()
+        self.output_words.clear()
+    
+    def reinit(self):
+        pass
+
+    def enqueue(self, *values):
+        self.input_queue.extend(values)
+        return self
+
+    def input(self, prompt = "Enter data >"):
+        if len(self.input_queue) > 0:
+            result = self.input_queue.pop(0)
+            page.terminal.innerHTML += f'<div class="input">{prompt} {result}</div>'
+        else:
+            result = ""
+        return result
+
+    def print(self, *args, **kwargs):
+        result = io.StringIO()
+        print(*args, file=result, **kwargs)
+        result = result.getvalue()
+        page.terminal.innerHTML += f'<div class="output">{result}</div>'
+        self.output_words.extend(result.lower().split())
+        return None
+
+    def get_terminal(self):
+        return " ".join(self.output_words)
+
+    def get_turtle_history(self):
+        history = window.turtle.getHistory().to_dict()
+        return history["angles"], history["lengths"]
+
+    def check_regular_polygon(self, num_sides):
+        angles, lengths = self.get_turtle_history()
+
+        if len(angles) != num_sides: return False
+        angle = 360 / num_sides
+        length = lengths[0]
+
+        if length == 0: return False
+
+        for i in range(1, num_sides):
+            if angles[i] - angles[i - 1] != angle: return False
+            if lengths[i] != length: return False
+
+        return True
+
+    def set_complete(self, value):
+        self.is_complete = value
+        self.update_option()
+        return value
+
+#############################################################################
+## Questions                                                               ##
+#############################################################################
+
+class Question01(Question):
+    def __init__(self):
+        super().__init__("Simple printing", """
+            <p>Print the text 'Hello world' on the screen.</p>
+        """)
+
+    def check(self):
+        text = self.get_terminal()
+        return self.set_complete("hello world" in text)
+
+class Question02(Question):
+    def __init__(self):
+        super().__init__("Input and output", """
+            <p>Ask for the user's name, then say hello to them by name.</p>
+        """)
+
+    def reinit(self):
+        id = random.randint(1, 10000)
+        self.name = f"Human {id}"
+        self.enqueue(self.name)
+                
+    def check(self):
+        result = self.get_terminal()
+        print(result)
+        return result == f"hello {self.name}".lower()
+
+class Question03(Question):
+    def __init__(self):
+        super().__init__("Inputting numbers", """
+            <p>Ask the user to enter two numbers. Then display the sum of the numbers.</p>
+        """)
+
+    def reinit(self):
+        self.a = random.randint(1, 100)
+        self.b = random.randint(1, 100)
+        self.enqueue(self.a, self.b)
+                
+    def check(self):
+        result = self.get_terminal()
+        return int(result) == self.a + self.b
+
+class Question04(Question):
+    def __init__(self):
+        super().__init__("Counting from zero", """
+            <p>Print the numbers from 0 to 9 on the screen.</p>
+        """)
+        
+    def check(self):
+        values = [ int(word) for word in self.get_terminal().split() ]
+        return values == list(range(10))
+
+class Question05(Question):
+    def __init__(self):
+        super().__init__("Counting from 1", """
+            <p>Print the numbers from 1 to 100 on the screen.</p>
+        """)
+        
+    def check(self):
+        values = [ int(word) for word in self.get_terminal().split() ]
+        return values == list(range(1, 101))
+              
+class Question06(Question):
+    def __init__(self):
+        super().__init__("Counting in steps", """
+            <p>Print all the numbers in the 7 times table from 7 to 700.</p>
+        """)
+        
+    def check(self):
+        values = [ int(word) for word in self.get_terminal().split() ]
+        return values == list(range(7, 707, 7))
+
+class Question07(Question):
+    def __init__(self):
+        super().__init__("Counting while omitting numbers", """
+            <p>Print all numbers from 7 to 700 <b>except</b> for numbers which are divisible by 7.</p>
+        """)
+        
+    def check(self):
+        values = [ int(word) for word in self.get_terminal().split() ]
+        return values == [ x for x in range(7, 707) if x % 7 != 0 ]
+        
+class Question08(Question):
+    def __init__(self):
+        super().__init__("Adding up numbers", """
+            <p>Ask the user to enter 10 numbers. Then print the total of all 10 numbers.</p>
+        """)
+        
+    def reinit(self):
+        self.total = 0
+        for count in range(10):
+            value = random.randint(1, 30)
+            self.total += value
+            self.enqueue(value)
+        
+    def check(self):
+        total = self.get_terminal()
+        return int(total) == self.total
+
+class Question09(Question):
+    def __init__(self):
+        super().__init__("Draw a square", """
+            <p>Use the turtle to draw a square.</p>
+        """, use_turtle = True)
+        
+    def check(self):
+        return self.check_regular_polygon(4)
+
+class Question10(Question):
+    def __init__(self):
+        super().__init__("Draw a hexagon", """
+            <p>Use the turtle to draw a regular hexagon.</p>
+        """, use_turtle = True)
+        
+    def check(self):
+        return self.check_regular_polygon(6)
+        
+#############################################################################
+## Other stuff                                                             ##
+#############################################################################
+
+class Turtle:
+    def __init__(self, js_turtle):
+        self.js_turtle = js_turtle
+        js_turtle.reset()
+
+    def left(self, degrees):
+        self.js_turtle.turn(-degrees)
+
+    def right(self, degrees):
+        self.js_turtle.turn(degrees)
+
+    def forward(self, length):
+        self.js_turtle.move(length)
+
+    def backward(self, length):
+        self.js_turtle.move(-length)
+
+def update_score():
+    instances = Question.instances
+    max_score = len(instances)
+    score = 0
+    for instance in instances:
+        if instance.is_complete: score += 1
+    document["score-text"].textContent = f"{score}/{max_score}"
+
+def clear(event = None):
+    question = Question.instance()
+    page.editor.session.setValue(window.localStorage.getItem(question.name) or "")
+    page.question_text.innerHTML = Question.instance().html
+    page.terminal.innerHTML = ""   
+    page.canvas.classList.toggle("hidden", not question.use_turtle)
+    hide_popup()
+    update_score()
+
+def show_popup(text):
+    page.stderr.textContent = text
+    page.popup_layer.setAttribute("class", "")
+
+def hide_popup(*args):
+    page.popup_layer.setAttribute("class", "hidden")
+
+def run_program(*args):
+    question = Question.instance()
+    success = False
+    text = page.editor.getValue()
+    window.localStorage.setItem(question.name, text.strip())
+    
+    clear()
+    question.clear()
+    question.reinit()
+
+    local_vars = {
+        "input" : question.input,
+        "print" : question.print,
+        "turtle" : Turtle(window.turtle)
+    }
+
+    try:
+        exec(text, globals(), dict(local_vars))
+    except Exception as e:
+        text = traceback.format_exc()
+        show_popup(text)
+
+    try:
+        success = Question.instance().check()
+    except Exception as e:
+        print(e)
+
+    if success:
+        Question.instance().set_complete(True)
+        update_score()
+        show_popup("Congratulations! You have finished this question!")
+
+def main():
+    window.ace.config.set("basePath", "https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.13/")
+
+    page.question_select = document["question-select"]
+    page.question_text = document["question-text"]
+    page.run_button = document["run-button"]    
+    page.stderr = document["stderr"]
+    page.popup_layer = document["popup-layer"]
+    page.editor = window.ace.edit("editor")
+    page.terminal = document["terminal"]
+    page.canvas = document["turtle-canvas"]
+   
+    page.popup_layer.addEventListener("click", hide_popup)
+    
+    page.editor.setOptions({
+        "mode": 'ace/mode/python',
+    });
+
+    page.question_select.bind("change", clear)
+    page.run_button.bind("click", run_program)
+    
+    Question01()
+    Question02()
+    Question03()
+    Question04()
+    Question05()
+    Question06()
+    Question07()
+    Question08()
+    Question09()
+    Question10()
+    clear()
+
+main()
---
+++ python1/turtle.mjs
@@ -0,0 +1,166 @@
+class Vec2D {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+    addVec2D(v) {
+        return new Vec2D(this.x + v.x, this.y + v.y)
+    }
+
+    mul(s) {
+        return new Vec2D(this.x * s, this.y * s)
+    }
+
+    rotate(degrees) {
+        const radians = Math.PI * degrees / 180
+        const sin = Math.sin(radians)
+        const cos = Math.cos(radians)
+        const x = this.x * cos - this.y * sin
+        const y = this.x * sin + this.y * cos
+        return new Vec2D(x, y)
+    }
+}
+
+class Turtle {
+    constructor(canvas) {
+        this.canvas = canvas
+        this.saveImage = undefined
+
+        const size = 12
+        this.points = [
+            new Vec2D(0, size).rotate(150),
+            new Vec2D(0, size * 0.7).rotate(180),
+            new Vec2D(0, size).rotate(210),
+        ]
+
+        this.reset()
+
+        window.setInterval(event => {
+            this.render()
+        }, 20)
+    }
+
+    getHistory() {
+        const angles = Array.from(this.history.keys())
+        const lengths = []
+        angles.sort((a, b) => parseInt(a) > parseInt(b))
+        for (let angle of angles) {
+            lengths.push(this.history.get(angle))
+        }
+        return { angles, lengths }
+    }
+
+    enqueueState() {
+        this.stateQueue.push({
+            position: this.position,
+            bearing: this.bearing
+        })
+        return this
+    }
+
+    reset() {
+        this.history = new Map()
+        this.saveImage = undefined
+		this.position = new Vec2D(0, 0)
+		this.bearing = 0
+        this.stateQueue = []
+        this.enqueueState()
+        const ctx = this.canvas.getContext("2d")
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+
+	turn(degrees) {
+		const newBearing = this.bearing + degrees
+        return this.setBearing(newBearing)
+	}
+
+	setPos(x, y) {
+		this.position = new Vec2D(x, y)
+        return this
+	}
+
+    setBearing(newBearing) {
+        let oldBearing = this.bearing
+        newBearing = newBearing >= 0 ? newBearing % 360 : 360 - (-newBearing) % 360
+
+        if (newBearing < oldBearing ) {
+            const alt = 360 + newBearing
+            if (alt - oldBearing < oldBearing - newBearing) {
+                newBearing = alt
+            }
+        } else {
+            const alt = 360 + oldBearing
+            if (alt - newBearing < newBearing - oldBearing) {
+                oldBearing = alt
+            }
+        }
+
+        const step = 10 / Math.abs(newBearing - oldBearing)
+        for (let scale = 0; scale < 1; scale += step) {
+            this.bearing = newBearing * scale + oldBearing * (1 - scale)
+            this.enqueueState()
+        }
+
+        this.bearing = newBearing % 360
+        return this.enqueueState()
+    }
+
+	move(length) {
+        const oldPosition = this.position
+        const newPosition = new Vec2D(0, length).rotate(-this.bearing).addVec2D(oldPosition)
+
+        const step = 4 / length
+        for (let scale = 0; scale < 1; scale += step) {
+            this.position = newPosition.mul(scale).addVec2D(oldPosition.mul(1 - scale))
+            this.enqueueState()
+        }
+
+        this.position = newPosition
+
+        const value = this.history.get(this.bearing) ?? 0
+        this.history.set(this.bearing, value + length)
+        return this.enqueueState()
+	}
+
+	render() {
+        const queue = this.stateQueue
+        const state0 = queue.length > 1 ? queue.shift() : queue[0]
+        const state1 = queue[0]
+
+        const ctx = this.canvas.getContext("2d")
+        const cx = this.canvas.width / 2
+        const cy = this.canvas.height / 2
+
+        if (this.saveImage) {
+            ctx.putImageData(this.saveImage, 0, 0)
+        }
+
+        ctx.lineWidth = 2
+        ctx.beginPath()
+        ctx.moveTo(cx + state0.position.x, cy - state0.position.y)
+        ctx.lineTo(cx + state1.position.x, cy - state1.position.y)
+
+        ctx.strokeStyle = "black"
+        ctx.stroke()
+
+        this.saveImage = ctx.getImageData(0, 0, this.canvas.width, this.canvas.height)
+
+        ctx.beginPath()
+        ctx.moveTo(cx + state1.position.x, cy - state1.position.y)
+
+		for (let point of this.points) {
+			point = point.rotate(-state1.bearing).addVec2D(state1.position)
+            ctx.lineTo(cx + point.x, cy - point.y)
+		}
+
+        ctx.closePath()
+
+        ctx.fillStyle = "black"
+        ctx.fill()
+        ctx.strokeStyle = "white"
+        ctx.stroke()
+	}
+}
+
+window.turtle = new Turtle(document.querySelector("#turtle-canvas"))
